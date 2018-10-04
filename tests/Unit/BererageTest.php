7d0b9d8d0567ba874557200f614cf3f047d9d3e0
shubham <shubhamsurya95@gmail.com>
2018-10-05 01:13:21 +0530

    user check with exception

--- tests/Unit/BererageTest.php
+++ tests/Unit/BererageTest.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Beverage;
+use App\User;
+use App\Exceptions\MinorCannotBuyAlcoholicBeverageException;
 class BererageTest extends TestCase
 {
 
@@ -36,5 +38,27 @@ class BererageTest extends TestCase
     public function beverage_has_type()
     {
         $this->assertNotEmpty($this->beverage->type);
+    }
+
+     /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    public function buy_alcohol_with_login()
+    {
+        $beverage = factory(Beverage::class)->make([
+            'type' => 'alcoholic'
+        ]);
+
+        $user = factory(User::class)->make([
+            'age' => '20'
+        ]);
+
+        $this->actingAs($user);
+
+        $this->expectException(MinorCannotBuyAlcoholicBeverageException::class);
+
+        $beverage->buy();   
     }
 }
